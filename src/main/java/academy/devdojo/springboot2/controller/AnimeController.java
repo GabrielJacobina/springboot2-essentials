b3427efebf8a3a69e2331efc4af721b8ec102971
Gabriel Jacobina <sgabrieljacobina@gmail.com>
2021-01-10 21:19:21 -0300

    Project with spring.start.io

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -22,6 +22,6 @@ public class AnimeController {
     @GetMapping(path = "list")
     public List<Anime> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"), new Anime("Berserk"));
+        return List.of(new Anime("Boku no Hero"), new Anime("Berserk"));
     }
 }
